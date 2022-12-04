b7fd8511afa4e6633c9cd30f6ee1fdce4cca9446
daanjiri <62439769+daanjiri@users.noreply.github.com>
2022-12-04 05:15:42 +0000

    checkboxes

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react';
 import * as d3 from 'd3';
 import dataCSV from './data_short.csv';
-import LineChart from './components/LineChart';
-
+import LineChart from './components/Linechart/LineChart';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import Checkbox from '@mui/material/Checkbox';
+import Box from '@mui/material/Box';
 
 
 const App=()=> {
@@ -20,6 +23,12 @@ const [data, setData] = useState([])
   // console.log(data)
   return (
     <div >
+      <div> <Checkbox
+      checked={checked}
+      onChange={handleChange}
+      inputProps={{ 'aria-label': 'controlled' }}
+    /> sexo1</div>
+   
       <LineChart data={data}/>
       {/* {data.map((d)=><div>{d['@_id']}</div>)} */}
     </div>
