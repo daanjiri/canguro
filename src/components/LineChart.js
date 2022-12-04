ed7a7588c43c888a382239dda9590e178e82b434
daanjiri <62439769+daanjiri@users.noreply.github.com>
2022-12-04 04:42:48 +0000

    added mui

--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -1,6 +1,7 @@
 import React,{ useEffect, useState, useRef } from 'react'
 import { Line } from 'react-chartjs-2';
 import 'chart.js/auto';
+import 'chartjs-plugin-zoom';
 
 const LineChart = ({data}) => {
   const ref = useRef();
@@ -9,7 +10,6 @@ const LineChart = ({data}) => {
 
     const nacimiento = d['ERN_Peso'] === '#NULL!'?0: parseInt(d['ERN_Peso'],10)
     const hospNeonatal = d['HD_PesoSalida'] === '#NULL!'?0:parseInt(d['HD_PesoSalida'],10)
-    const entrada = d['V234']=== '#NULL!'?0:parseInt(d['V234'],10)
     const sem40 = d['V218']=== '#NULL!'?0:parseInt(d['V218'],10)
     const mes3 = d['V261']=== '#NULL!'?0:parseInt(d['V261'],10)
     const mes6 = d['V304']=== '#NULL!'?0:parseInt(d['V304'],10)
@@ -20,7 +20,7 @@ const LineChart = ({data}) => {
       {
         id:d['@_id'],
         label:`paciente:${d['@_id']} `,
-        data:[nacimiento,hospNeonatal,entrada,sem40,mes3,mes6,mes9,mes12]
+        data:[nacimiento,hospNeonatal,sem40,mes3,mes6,mes9,mes12]
       }
     )
   })
@@ -32,8 +32,15 @@ const LineChart = ({data}) => {
       <Line ref={ref}
       datasetIdKey='id'
       data={{
-        labels: ['nacimiento','hospNeonatal','entrada','sem40','mes3','mes6','mes9','mes12'],
+        labels: ['nacimiento','hospNeonatal','sem40','mes3','mes6','mes9','mes12'],
         datasets: PesoDataset,
+      }}
+      options={{
+        legend: {
+          display: false
+       },
+        // title: { display: true, text: 'My Chart' },
+        responsive:true,
       }}
       />
     </div>
